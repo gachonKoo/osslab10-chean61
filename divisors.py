ab45938dcecb59ac435f6bc77268cf3770430d69
chean61 <chean06@gachon.ac.kr>
2025-11-09 18:51:26 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,8 +1,7 @@
 import sys
-if len(sys.argv) > 1:
-    n = int(sys.argv[1])
-else:
-    n = int(input())
-for i in range(1, n + 1):
-    if n % i == 0:
+
+number = int(sys.argv[1])
+
+for i in range(1, number + 1):
+    if number % i == 0:
         print(i, end=" ")
